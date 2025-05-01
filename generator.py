2a580f25136bcdc0d8816b90f73f2da3bf84b233
Yuan Chen <chenyuan@Yuans-MacBook-Air-2020.local>
2025-05-01 22:35:42 +0900

    get rid of index.hmtl include itself.

--- generator.py
+++ generator.py
@@ -6,7 +6,7 @@ def generate_index():
     env = Environment(loader=FileSystemLoader("templates"))
     template = env.get_template("index.html.j2")
     files = sorted(os.listdir(PAGE_DIR), reverse=True)
-    html_files = [f for f in files if f.endswith(".html")]
+    html_files = [f for f in files if f.endswith(".html") and not f.startswith("index")]
     rendered = template.render(news_pages=html_files)
     with open(os.path.join(PAGE_DIR, "index.html"), "w", encoding="utf-8") as f:
         f.write(rendered)
